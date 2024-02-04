e8203b430ca54d4ead266fd731c78b57c3796479
OGATA Hiromu <hiromukkun@gmail.com>
2024-02-04 17:17:42 +0900

    add:第7回ドリルで用いたExampleクラスを追加

---
+++ server/src/Example.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Example {
+  public static void main(String[] args) {
+    Set<String> set = new HashSet<String>();
+    List<String> list = new LinkedList<String>();
+    Scanner input = new Scanner(System.in);
+    for (String s : args) { System.out.println(s); } 
+  }
+}
